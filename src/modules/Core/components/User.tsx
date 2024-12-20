7c11ce8b63b0c076da90e1a43dfa436c6253753a
Maurizio Battaghini <maurizio.battaghini+tech@gmail.com>
2024-12-21 00:32:48 +0100

    minor

--- src/modules/Core/components/User.tsx
+++ src/modules/Core/components/User.tsx
@@ -26,6 +26,11 @@ function User() {
     onError: (error) => console.error("Login Failed:", error),
   });
 
+  const handleGoogleLogout = () => {
+    googleLogout();
+    clearUser();
+  };
+
   useEffect(() => {
     if (userToken && !user) {
       axios
@@ -49,9 +54,7 @@ function User() {
               className="h-10 w-10 border-spacing-2 overflow-hidden rounded-full"
               src={hasImgError ? userAvatarPlaceholder : user.picture}
               alt="user avatar"
-              onError={(e) => {
-                setHasImgError(true);
-              }}
+              onError={() => setHasImgError(true)}
             />
 
             <span className="absolute bottom-0 left-7 z-10 h-3.5 w-3.5 translate-y-1/4 transform rounded-full border-2 border-white bg-green-400"></span>
@@ -59,13 +62,7 @@ function User() {
 
           {user && isMenuOpen && (
             <span onMouseLeave={toggleMenu}>
-              <UserMenu
-                data={user}
-                onLogout={() => {
-                  googleLogout();
-                  clearUser();
-                }}
-              />
+              <UserMenu data={user} onLogout={handleGoogleLogout} />
             </span>
           )}
         </>
@@ -74,11 +71,9 @@ function User() {
           role="button"
           title="google login"
           aria-label="google login"
-          className="inline-flex items-center rounded bg-[#4285F4] px-4 py-2 text-center text-xs font-medium text-white hover:bg-[#4285F4]/90"
+          className="inline-flex cursor-pointer items-center rounded bg-[#4285F4] px-4 py-2 text-center text-xs font-medium text-white hover:bg-[#4285F4]/90"
           type="button"
-          onClick={() => {
-            handleGoogleLogin();
-          }}
+          onClick={() => handleGoogleLogin()}
         >
           <img className="me-2 h-4 w-4" src={googleLogoSvg} alt="google logo" />
           Sign in with Google
